8424522081eb07f0147b91aff581f858cc4c2135
Ellis Delgado <ellisdelgado13@gmail.com>
2021-07-19 12:10:56 -0700

    Created user dao

---
+++ project0/src/main/java/com/revature/data/UserDAO.java
@@ -0,0 +1,10 @@
+package com.revature.data;
+
+
+import com.revature.beans.User;
+import com.revature.beans.UserType;
+
+//the user database access object or DAO will allow us to access users 
+public class UserDAO {
+
+}
